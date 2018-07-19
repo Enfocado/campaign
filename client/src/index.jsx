775a5fb2752eaf1654ef71e045f4d4f53abc9cf6
Diego Espinoza <diespinozas@gmail.com>
2018-07-19 13:13:27 -0700

    set up basic index.jsx file

---
+++ client/src/index.jsx
@@ -0,0 +1,11 @@
+/* eslint-env browser */
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const App = () => (
+  <div>
+    Helo Webpack
+  </div>
+);
+
+ReactDOM.render(<App />, document.getElementById('app'));
