b81e161cc7a2fe5b6cfcd76d0393ab9935652184
Diego Espinoza <diespinozas@gmail.com>
2018-07-19 13:30:24 -0700

    fix syntax webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const path = require('path');
 const SRC_DIR = path.join(__dirname, '/client/src');
 const DIST_DIR = path.join(__dirname, '/client/dist');
 
-module.esports = {
+module.exports = {
   entry: `${SRC_DIR}/index.jsx`,
   output: {
     filename: 'bundle.js',
