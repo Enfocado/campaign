960cea30f43ba370ea1a2663aa62767515426737
Diego Espinoza <diespinozas@gmail.com>
2018-07-20 10:52:54 -0700

    set up test suite with jest

--- server/index.js
+++ server/index.js
@@ -3,5 +3,16 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 3002;
 
+
 app.use(express.static('client/dist'));
-app.listen(port, () => console.log(`listening to ${port}`));
+
+app.get('/test', (req, res) => {
+  res.send('hello GET');
+});
+
+if (process.env.NODE_ENV !== 'test') {
+  app.listen(port, () => console.log(`listening to ${port}`));
+}
+
+
+module.exports = app;
