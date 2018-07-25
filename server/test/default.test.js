a1ee3f3f22c984c5a955c1efae0c55109e1072bb
Diego Espinoza <diespinozas@gmail.com>
2018-07-25 16:53:08 -0700

    keep base test on suite

--- server/test/default.test.js
+++ server/test/default.test.js
@@ -3,13 +3,11 @@ const app = require('../index.js');
 
 describe('Test /', () => {
   it('should return status code 200', async () => {
-    const response = await request(app).get('/project/1/section/about_components');
+    const response = await request(app).get('/test');
     expect(response.statusCode).toBe(200);
   });
   it('should return text hello GET', async () => {
-    const response = await request(app).get('/project/1/section/about_components');
-    request(app).get('/project/1/section/about_components', (data) => {
-      expect(response.text).toBe(data);
-    });
+    const response = await request(app).get('/test');
+    expect(response.text).toBe('hello GET');
   });
 });
