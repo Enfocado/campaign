0126f4510f03e3cc77c2c1a5307c39be34b1046a
Diego Espinoza <diespinozas@gmail.com>
2018-07-25 16:30:05 -0700

    refactor test for about section get request

--- server/test/default.test.js
+++ server/test/default.test.js
@@ -3,11 +3,13 @@ const app = require('../index.js');
 
 describe('Test /', () => {
   it('should return status code 200', async () => {
-    const response = await request(app).get('/test');
+    const response = await request(app).get('/project/1/section/about_components');
     expect(response.statusCode).toBe(200);
   });
   it('should return text hello GET', async () => {
-    const response = await request(app).get('/test');
-    expect(response.text).toBe('hello GET');
+    const response = await request(app).get('/project/1/section/about_components');
+    request(app).get('/project/1/section/about_components', (data) => {
+      expect(response.text).toBe(data);
+    });
   });
 });
