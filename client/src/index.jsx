26e4fb4fe3e9b64ead4ad4d959ea9b37038f0e40
Diego Espinoza <diespinozas@gmail.com>
2018-07-26 16:56:55 -0700

    add pledges section to main component

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -2,10 +2,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import About from './components/about';
+import Pledges from './components/pledges';
 
 const App = () => (
   <div>
+    <div>
     <About />
+    </div>
+    <div>
+      <Pledges />
+    </div>
   </div>
 );
 
