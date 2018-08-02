2366cbe60c4730bbf0a3474eeb1829b9305fcf4c
Diego Espinoza <diespinozas@gmail.com>
2018-08-01 18:59:55 -0700

    circle ci fix

--- server/test/server.test.js
+++ server/test/server.test.js
@@ -2,8 +2,8 @@ const request = require('supertest');
 const { connection } = require('../../db/index');
 const app = require('../index.js');
 
-describe('Test /', () => {
-  test('should return status code 200', async () => {
+describe('Test /project/1/section/about_components', () => {
+  it('should return status code 200', async () => {
     const response = await request(app).get('/project/1/section/about_components');
     expect(response.statusCode).toBe(200);
     connection.end();
