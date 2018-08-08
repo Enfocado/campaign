0b97448056fd0b329f487b6ad85aaf2af7d2b275
Diego Espinoza <diespinozas@gmail.com>
2018-08-07 17:18:46 -0700

    styling improvement

--- client/src/components/pledge.jsx
+++ client/src/components/pledge.jsx
@@ -13,6 +13,15 @@ const Main = styled.div`
 const PledgeMain = styled.div`
   margin: 0 20px;
   padding: 20px 0;
+  > h3 {
+    margin: 0 0 0 0px;
+    font-weight: 400;
+    font-size: 18px;
+  }
+  > p {
+    font-size: 14px;
+    line-height: 18px;
+  }
 `;
 
 const TitleTwo = styled.h2`
@@ -27,6 +36,16 @@ const PledgeDetails = styled.div`
   width: 50%;
   color: #020621;
   float: left;
+  > span {
+    color: #656969;
+    font-size: 10px;
+    font-weight: 500;
+    letter-spacing: 0.5epx;
+  }
+  > p {
+    font-size: 14px;
+    margin: 0;
+  }
 `;
 
 const Button = styled.button`
@@ -36,6 +55,7 @@ const Button = styled.button`
   background-color: #009E74;
   color: #FFFFFF !important;
   height: 42px;
+  cursor: pointer;
 `;
 
 const Table = styled.table`
@@ -69,89 +89,151 @@ const Backers = styled.span`
   color: #020621;
 `;
 
+const Hover = styled.div`
+  display: block;
+  background: rgba(0,158,116);
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  height: 100%;
+  width: 100%;
+  opacity: 0;
+  transition: .5s ease;
+  cursor: pointer;
+  > div {
+    > p {
+      color: white;
+      position: absolute;
+      top: 50%;
+      left: 50%;
+      -ms-transform: translate(-50%, -50%);
+      transform: translate(-50%, -50%);
+      text-align: center;
+      padding: 0;
+      margin: 0;
+      font-size: 16px;
+    }
+  }
+`;
+
+const Container = styled.div`
+  position: relative;
+  &:hover {
+    > #hove {
+      opacity: 0.9;
+    }
+  }
+`;
+
 class Pledge extends React.Component {
   constructor(props) {
     super(props);
     const { pledge } = this.props;
     this.state = {
       element: pledge,
+      selected: false,
     };
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick() {
+    this.setState({
+      selected: true,
+    });
   }
 
   render() {
     const { element } = this.state;
+    const { selected } = this.state;
     return (
-      <Main key={element.id}>
-        <PledgeMain>
-          <TitleTwo>
-            Pledge $
-            {element.base_amount}
-            {' '}
-            or more
-          </TitleTwo>
-          <h3>
-            {element.title}
-          </h3>
-          <p>
-            {element.description}
-          </p>
-          <div>
-            <PledgeDetails>
-              <span>
-                ESTIMATED DELIVERY
-              </span>
-              <br />
+      <Container>
+        {selected === false && (
+          <Hover id="hove" onClick={this.handleClick}>
+            <div>
               <p>
-                {moment(element.delivery_date).format('MMM YYYY')}
+                Select this reward
               </p>
-            </PledgeDetails>
-            <PledgeDetails>
-              <span>
-                SHIPS TO
-              </span>
-              <br />
-              <p>
-                {element.ships_to}
-              </p>
-            </PledgeDetails>
-            <br />
-            <Backers>
-              {element.max_backers}
+            </div>
+          </Hover>
+        )}
+        <Main key={element.id}>
+          <PledgeMain>
+            <TitleTwo>
+              Pledge $
+              {element.base_amount}
               {' '}
-              backers
-            </Backers>
-          </div>
-          <div>
-            <Label>
-              Shipping Destination
-            </Label>
-            <br />
-            <CountrySelector />
-          </div>
-          <div>
-            <Label>
-              Pledge amount
-            </Label>
-            <Table>
-              <tbody>
-                <tr>
-                  <Currency>
-                    $
-                  </Currency>
-                  <td>
-                    <form>
-                      <InputAmount type="number" />
-                    </form>
-                  </td>
-                </tr>
-              </tbody>
-            </Table>
-          </div>
-          <Button type="submit">
-            Continue
-          </Button>
-        </PledgeMain>
-      </Main>
+              or more
+            </TitleTwo>
+            <h3>
+              {element.title}
+            </h3>
+            <p>
+              {element.description}
+            </p>
+            <div>
+              <PledgeDetails>
+                <span>
+                  ESTIMATED DELIVERY
+                </span>
+                <br />
+                <p>
+                  {moment(element.delivery_date).format('MMM YYYY')}
+                </p>
+              </PledgeDetails>
+              <PledgeDetails>
+                <span>
+                  SHIPS TO
+                </span>
+                <br />
+                <p>
+                  {element.ships_to}
+                </p>
+              </PledgeDetails>
+              <br />
+              <Backers>
+                {element.max_backers}
+                {' '}
+                backers
+              </Backers>
+            </div>
+            {selected === true && (
+              <div>
+                <div>
+                  <Label>
+                    Shipping Destination
+                  </Label>
+                  <br />
+                  <CountrySelector />
+                </div>
+                <div>
+                  <Label>
+                    Pledge amount
+                  </Label>
+                  <Table>
+                    <tbody>
+                      <tr>
+                        <Currency>
+                          $
+                        </Currency>
+                        <td>
+                          <form>
+                            <InputAmount type="number" />
+                          </form>
+                        </td>
+                      </tr>
+                    </tbody>
+                  </Table>
+                </div>
+                <Button type="submit">
+                  Continue
+                </Button>
+              </div>
+            )}
+          </PledgeMain>
+        </Main>
+      </Container>
     );
   }
 }
